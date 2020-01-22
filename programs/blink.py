6b4bb5ef58a5478d3118ec8fe97ca4e915c85bb7
Perry Kivolowitz <perry@kivolowitz.com>
2020-01-22 10:29:23 -0600

    updated the way strings are formatted

--- programs/blink.py
+++ programs/blink.py
@@ -7,7 +7,7 @@ current_index = 0
 key = ''
 while key != 'q':
     cv2.imshow('Pluto', images[current_index])
-    cv2.setWindowTitle('Pluto', str.format('Pluto Image: %d   -   \'q\' to quit' % current_index))
+    cv2.setWindowTitle('Pluto', 'Pluto Image: {:d}   -   \'q\' to quit'.format(current_index))
     key = chr(cv2.waitKey(0))
     current_index = 1 - current_index
 cv2.destroyAllWindows()
